dfe0c338864dde50bae6f2465e13bcb924037c42
Clara Burgard <clara.burgard@gmail.com>
2022-04-26 19:18:28 +0200

    new try references

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ extensions = [
 	'sphinxcontrib.bibtex'
 ]
 
+bibtex_bibfiles = './literature/references.bib'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
