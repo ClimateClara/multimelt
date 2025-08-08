a55545edd90495f190325b93826a97e9b73a361f
Clara Burgard <clara.burgard@gmail.com>
2025-08-08 12:24:25 +0200

    tried to change the style to the default rtd style

--- docs/conf.py
+++ docs/conf.py
@@ -59,8 +59,12 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 # html_theme = 'alabaster'
+
+# added by Clara
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
